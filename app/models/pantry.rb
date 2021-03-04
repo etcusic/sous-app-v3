c54ba7a531bb05bd52a804cc2fbeea51b5e219d7
Eli Cusic <tntitans31@gmail.com>
2021-03-04 09:31:21 -0600

    set up a total_cost method in pantry and added info to pantry show page

--- app/models/pantry.rb
+++ app/models/pantry.rb
@@ -3,4 +3,9 @@ class Pantry < ApplicationRecord
   has_many :consumables, dependent: :destroy
   alias_attribute :ingredients, :consumables
 
+  # should this be in a module? - both recipe and pantry use this
+  def total_cost
+    self.ingredients.map{ | ing | ing.cost_per_unit * ing.quantity } .reduce(&:+).round(2)
+  end
+
 end
