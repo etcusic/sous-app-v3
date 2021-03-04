04f66acb30978912556d490fd6de4c9811e7f9b4
Eli Cusic <tntitans31@gmail.com>
2021-03-04 11:08:11 -0600

    added ingredient checker in recipe model to check against pantry

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,4 +6,15 @@ class Recipe < ApplicationRecord
     self.ingredients.map{ | ing | ing.quantity * ing. cost_per_unit } .reduce(&:+).round(2)
   end
 
+  def enough_in_pantry?
+    pantry = self.user.pantry
+    if self.ingredients.all?{|ing| ing.quantity <= pantry.ingredients.find_by(name: ing.name).quantity}
+      # be more specific with how many portions of recipe can be made
+      "(You have enough ingredients in the pantry for this recipe!)"
+    else
+      # be more specific with which ingredients need to be stocked
+      "(Not enough ingredients in pantry for this recipe)"
+    end
+  end
+
 end
