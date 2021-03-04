9873e476c5c2026e8cced147bf8b345043fff05b
Eli Cusic <tntitans31@gmail.com>
2021-03-04 09:10:07 -0600

    set up pantry to display table of supplies and quantity

--- app/controllers/pantries_controller.rb
+++ app/controllers/pantries_controller.rb
@@ -1,2 +1,13 @@
 class PantriesController < ApplicationController
+
+    def show
+        @pantry = Pantry.find_by_id(pantry_params)
+    end
+
+    private
+
+    def pantry_params
+        params.require(:id)
+    end
+
 end
