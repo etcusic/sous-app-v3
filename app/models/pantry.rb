1f6e1e8a6eb3fad42a1b7988a757efd7b703a5da
Eli Cusic <tntitans31@gmail.com>
2021-03-04 10:15:59 -0600

    added available_recipes method to pantry classes and inserted output with links into show page

--- app/models/pantry.rb
+++ app/models/pantry.rb
@@ -8,4 +8,12 @@ class Pantry < ApplicationRecord
     self.ingredients.map{ | ing | ing.cost_per_unit * ing.quantity } .reduce(&:+).round(2)
   end
 
+  # inefficient and belongs in module to share between recipe and pantry? 
+  # also => should recipe ingredients belong to pantry ingredients - or both belong to a parent/root ingredient??
+  def available_recipes
+    self.user.recipes.select do |recipe|
+      recipe.ingredients.all?{|ing| ing.quantity <= self.ingredients.find_by(name: ing.name).quantity}
+    end
+  end
+
 end
