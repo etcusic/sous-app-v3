c37c4a74d92820ec4a9338111f8ad86ff4882c7a
Eli Cusic <tntitans31@gmail.com>
2021-03-04 08:06:32 -0600

    began setting up recipe and pantry controllers and views

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,19 @@
 class RecipesController < ApplicationController
+
+    def index
+        params[:permitted] = true
+        @recipes = Recipe.find_by(user_id: params[:user_id])
+        binding.pry
+    end
+
+    def show
+        @recipe = Recipe.find_by_id(1)
+    end
+
+    private
+
+    def recipe_params
+        params.require(:id)
+    end
+
 end
