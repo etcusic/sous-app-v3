ef5d23efb13f51c8c87700305b0fc261a86c26d6
Eli Cusic <tntitans31@gmail.com>
2021-03-04 08:35:38 -0600

    filled out recipe show page a bit more and rounded recipe.total_cost

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,7 +3,7 @@ class Recipe < ApplicationRecord
   has_many :ingredients, dependent: :destroy
 
   def total_cost
-    self.ingredients.map{ | ing | ing.quantity * ing. cost_per_unit } .reduce(&:+)
+    self.ingredients.map{ | ing | ing.quantity * ing. cost_per_unit } .reduce(&:+).round(2)
   end
 
 end
